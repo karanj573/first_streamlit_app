949b46241a436aa5ba4350c0f5773a7a710176dc
karanj573 <90253129+karanj573@users.noreply.github.com>
2023-08-28 15:37:17 +0530

    Update streamlit_app.py
    Added header add 3 titles.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents New healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🍜Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍦Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑Hard-Boiled Free-Range Egg')
